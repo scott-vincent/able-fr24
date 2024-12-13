1e116177d7d1dd7411100fb280b6670172c20357
Scott Vincent <scottvincent@yahoo.com>
2024-12-13 18:35:27 +0000

    wip

--- backup.sh
+++ backup.sh
@@ -1,7 +1,7 @@
 projects="/data/share/projects"
 
-home="/home/pi/able_fr24"
-project="$projects/able_fr24"
+home="/home/pi/able-fr24"
+project="$projects/able-fr24"
 
 sudo rm -rf $project.old >/dev/null 2>/dev/null
 sudo mv $project $project.old
